15027bca2200daf641f0812428e05ed8d0369bbd
iamhnb <24110078@student.hcmute.edu.vn>
2026-08-20 17:08:18 +0700

    Add files via upload

---
+++ src/murach/data/UserDB.java
@@ -0,0 +1,10 @@
+package murach.data;
+
+import murach.business.User;
+
+public class UserDB {
+  public static long insert(User user) {
+    // Giả lập chức năng lưu dữ liệu người dùng
+    return 1;
+  }
+}
